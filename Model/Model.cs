e66603ba73f0b2e112e6d51434807ccdc965ef7c
Chandrakanth <cpasunuru@saventech.com>
2020-12-17 19:36:07 +0530

    project setup

---
+++ Model/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace todo.Model
+{
+    public class Model : DbContext
+    {
+       
+    }
+}
